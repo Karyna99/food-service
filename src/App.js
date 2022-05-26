10f43a02e28041b4914dbe427db0f0ab8b35a5bf
Karyna Krapiunitskaya <krapinah@gmail.com>
2022-05-26 21:21:27 +0300

    added icons to the Header component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Section from './Section';
 import Container from './Container';
 import Header from './Header';
 import Footer from './Footer';
-import Popup from './Popup';
-import { useState } from 'react';
 
 function App() {
   
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,6 +1,9 @@
 import "./style.css";
 import { useState } from "react";
 import Popup from "../Popup";
+import logo from "../images/logo.svg";
+import cart from "../images/cart.svg";
+import profile from "../images/profile.svg"
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -11,27 +14,29 @@ const Header = () => {
 
     return (
         <header className="header">
-            <ul className="header__nav">
-                <li><a className="header__link" href="#RU">RU</a></li>
-                <li><a className="header__link" href="#EN">EN</a></li>
-                <li><a className="header__link" href="#UA">UA</a></li>
-            </ul>
-            <ul className="header__nav">
-                <li><a className="header__link" href="#main">MAIN</a></li>
-                <li><a className="header__link" href="#shop">SHOP</a></li>
-                <li><a className="header__link" href="#about">ABOUT</a></li>
-                <li><a className="header__link" href="#faq">FAQ</a></li>
-                <li><a className="header__link" href="#contacts">CONTACTS</a></li>
-            </ul>
-            <ul className="header__nav">
-                <li>25$</li>
-                <li>a</li>
+            <div className="header__nav">
+                <ul className="header__links">
+                    <li><a className="header__link" href="#RU">RU</a></li>
+                    <li><a className="header__link" href="#EN">EN</a></li>
+                    <li><a className="header__link" href="#UA">UA</a></li>
+                </ul>
+                <img className="header__logo" src={logo} alt="logo"></img>
+                <ul className="header__links">
+                    <li><a className="header__link" href="#main">MAIN</a></li>
+                    <li><a className="header__link" href="#shop">SHOP</a></li>
+                    <li><a className="header__link" href="#about">ABOUT</a></li>
+                    <li><a className="header__link" href="#faq">FAQ</a></li>
+                    <li><a className="header__link" href="#contacts">CONTACTS</a></li>
+                </ul>
+            </div>
+            <ul className="header__loginAndCart">
+                <li className="header__price">25$</li>
+                <li><img src={cart} alt="cart"></img></li>
                 <li>
-                    <input
-                        type="button"
+                    <button
+                        className="header__loginButton"
                         value="Click to Open Popup"
-                        onClick={togglePopup}
-                    />
+                        onClick={togglePopup}><img src={profile} alt="profile"></img></button>
                     {isOpen && <Popup
                         closePopup={togglePopup}
                     />}
