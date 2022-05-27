04495ee2937a9839914ea7aa3eebb92fe9a03ac4
Karyna Krapiunitskaya <krapinah@gmail.com>
2022-05-27 10:31:11 +0300

    font change

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,13 +1,15 @@
 import "./style.css";
 
-const Section = ({ title, body, extraContent }) => (
-    <section className="section">
-        <div>{extraContent}</div>
-        <h2 className="section__header">{title}</h2>
-        <div className="section__div">
-            {body}
-        </div>
-    </section>
-);
+const Section = ({ title, body, extraContent }) => {
+    return (
+        <section className="section">
+            <div>{extraContent}</div>
+            <h2 className="section__header">{title}</h2>
+            <div className="section__div">
+                {body}
+            </div>
+        </section>
+    )
+};
 
 export default Section;
