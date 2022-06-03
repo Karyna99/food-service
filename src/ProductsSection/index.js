dfb9c4bbb11d462cb97c1fbf284a80b1a34728c7
Karyna Krapiunitskaya <krapinah@gmail.com>
2022-06-03 12:50:43 +0300

    styled the search area in ProductsSection

--- src/ProductsSection/index.js
+++ src/ProductsSection/index.js
@@ -1,18 +1,29 @@
 import "./style.css";
 import ProductsGrid from "../ProductsGrid";
+import search from "../images/Vector.svg"
 
 const ProductsSection = () => {
     return (
         <section className="product__section">
+            <div className="products__navigation">
             <div className="products__search">
-               <form>
-                   <input className="search__byInput"/>
-               </form>
-               <ul className="search__bySort">
-                   <li><button>FRUITS</button></li>
-                   <li><button>VEGETABLES</button></li>
-                   <li><button>FRESH BOXES</button></li>
-               </ul>
+                <div className="search__byInput">
+                    <button
+                        className="search__button">
+                        <img src={search} alt="" />
+                    </button>
+                    <input
+                        type="text"
+                        placeholder="SEARCH"
+                        className="search__input"
+                    />
+                </div>
+                <ul className="search__bySort">
+                    <li><button className="button__bySort">FRUITS</button></li>
+                    <li><button className="button__bySort">VEGETABLES</button></li>
+                    <li><button className="button__bySort">FRESH BOXES</button></li>
+                </ul>
+            </div>
             </div>
             <div className="products__grid">
                 <ProductsGrid />
