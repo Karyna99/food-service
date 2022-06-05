39fb16187f0a719939fac4ce8e910724eae953c8
Karyna Krapiunitskaya <krapinah@gmail.com>
2022-06-05 14:13:46 +0300

    created animation for arrows

--- src/AnimatedArrows/index.js
+++ src/AnimatedArrows/index.js
@@ -4,9 +4,9 @@ import arrow from "../images/arrow_next.svg";
 const AnimatedArrows = () => {
     return (
         <div className="div__arrows">
-            <img className="arrow" alt="" src={arrow}></img>
-            <img className="arrow" alt="" src={arrow}></img>
-            <img className="arrow" alt="" src={arrow}></img>
+            <img className="arrow1" alt="" src={arrow}></img>
+            <img className="arrow2" alt="" src={arrow}></img>
+            <img className="arrow3" alt="" src={arrow}></img>
         </div>
     );
 };
