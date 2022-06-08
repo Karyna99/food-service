2360c79906316b5bff5c772fc361c7e1a376314b
Karyna Krapiunitskaya <krapinah@gmail.com>
2022-06-08 15:18:19 +0300

    moved the array of progucts to separate file

---
+++ src/ProductsGrid/products.js
@@ -0,0 +1,46 @@
+const products = [
+    {
+        id: "fr_1",
+        category: "fruit",
+        name: "banana",
+        price: 2,
+        source: "https://i.ibb.co/0sL9DHT/banan-card.jpg"
+    },
+    {
+        id: "fr_2",
+        category: "fruit",
+        name: "watermelon",
+        price: 9,
+        source: "https://i.ibb.co/tMGT8vJ/mockup-graphics-13-PBli-WTDng-unsplash.jpg"
+    },
+    {
+        id: "fr_3",
+        category: "fruit",
+        name: "pear",
+        price: 5,
+        source: "https://i.ibb.co/6rp6zvp/pear-card.jpg"
+    },
+    {
+        id: "fr_4",
+        category: "fruit",
+        name: "orange",
+        price: 7,
+        source: "https://i.ibb.co/D8yKcp1/orange-card.jpg"
+    },
+    {
+        id: "fr_5",
+        category: "fruit",
+        name: "apple",
+        price: 3,
+        source: "https://i.ibb.co/KFqsJ69/apple-card.jpg"
+    },
+    {
+        id: "fr_6",
+        category: "fruit",
+        name: "strawberry",
+        price: 15,
+        source: "https://i.ibb.co/nm2Dqq7/strawberry-card.jpg"
+    }
+];
+
+export { products };
